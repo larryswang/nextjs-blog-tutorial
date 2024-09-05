9fe5b1150bb80432500ad48b8d7462837385eaeb
Larry (Shiyu) Wang <shiyuw@umich.edu>
2024-09-04 19:30:18 -0700

    An initial commit
    Add S3 design

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
           alt={"logo"}
         />
         <Link href="/">
-          <h1 className="text-2xl text-white font-bold mt-4">Jack's Blog</h1>
+          <h1 className="text-2xl text-white font-bold mt-4">Larry Wang's Blog</h1>
         </Link>
         <p className="text-slate-300">🤟 Welcome to my tech blog. 💻</p>
       </div>
@@ -28,7 +28,7 @@ export default function RootLayout({
   const footer = (
     <footer>
       <div className="border-t border-slate-400 mt-12 py-6 text-center text-slate-400">
-        <h3>Designed by Pixegami</h3>
+        <h3>email: shiyuw@umich.edu</h3>
       </div>
     </footer>
   );
---
+++ posts/aws-s3.md
@@ -0,0 +1,6 @@
+---
+title: "Design an AWS S3"
+subtitle: "System design of an object storage like AWS S3"
+date: "2024-09-04"
+---
+Some content TBD.
